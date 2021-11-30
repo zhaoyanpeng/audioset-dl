ae1564f83b39c179e742856b8841418815f261e1
yannzhao <yannzhao.ed@gmail.com>
2021-11-30 14:30:21 -0800

    small modifications for test

--- one_for_all.sh
+++ one_for_all.sh
@@ -8,9 +8,10 @@ echo $root $OUTPUT_PATH $1
 
 args=$1
 
-home=/home/yanpengz/data/
+home=/home/yanpengz/data/del
 root=$home
-args="--home $root --nprocess 45 --chunk_e 50 --portion unbalanced --peeprate 30"
+#args="--home $root --nprocess 45 --chunk_e 50 --portion unbalanced --peeprate 30"
+args="--home $root --keepdata true --nprocess 5 --chunk_e 5 --portion unbalanced --peeprate 3"
 nohup python one_for_all.py $args >> $root/one_for_all.out 2>&1 &
 
 #python one_for_all.py $args 
